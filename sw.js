b3293aeecbc78b86d87a8c3449d6a94a8e4ad2e5
Snail3D <ericwoodard84@gmail.com>
2026-02-14 02:05:21 -0700

    feat: add teal claw logo as PWA icons and favicon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'tealclaw-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'tealclaw-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png', '/favicon-32.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
